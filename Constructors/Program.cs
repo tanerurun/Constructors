0ece2f310a8a0e26c630a0a0297bde740fc4ff94
taner <taner@DESKTOP-8FBSMG9>
2021-09-03 22:42:09 +0300

    Add project files.

---
+++ Constructors/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer= new Customer();
+            customer.id=1;
+            customer.name = "Taner";
+
+            Console.ReadKey();
+        }
+    }
+    class Customer
+    {
+        public Customer(int id,string firstName,string lastName,string city)
+        {
+            id=id;
+            name=firstName;
+            lastName = lastName;
+            city = city;
+
+        }
+        //default constructors
+        public Customer()
+        {
+            Console.WriteLine("Yapıcı blok çalıştı");
+        }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string lastName { get; set; }
+        public string city { get; set; }
+    }
+}
